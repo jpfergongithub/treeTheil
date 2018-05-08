9c911b926054a4374eb81b95b5439046bcabbb1f
John-Paul Ferguson <jpferg@stanford.edu>
2018-05-07 19:18:06 -0700

    updated test_theil and treeTheil

--- test_theil.py
+++ test_theil.py
@@ -1,10 +1,25 @@
 import pandas as pd
 import numpy as np
 import treeTheil as tT
+import matplotlib.pyplot as plt
 
 bar = pd.DataFrame(pd.read_csv('test_data_multiyear.csv'))
 levels = ['year', 'city', 'firm']
 groups = ['white', 'black']
-theils = np.array([(year, tT.theil(tT.theil_tree(bar[bar['year'] == year],
-                                   levels, groups), str(year), 1))
+years = np.array([year for year in bar.year.unique()])
+theils = np.array([tT.theil(tT.theilTree(bar[bar['year'] == year],
+                                         levels, groups), str(year), 1)
                   for year in bar.year.unique()])
+btws = np.array([tT.btw_theil(tT.theilTree(bar[bar['year'] == year],
+                                           levels, groups), str(year))
+                for year in bar.year.unique()])
+wins = np.array([[tT.win_theil(tT.theilTree(bar[bar['year'] == year],
+                                            levels, groups), '%s|%s' % (
+                                                year, city))
+                  for year in bar.year.unique()]
+                 for city in bar.city.unique()])
+firmxs = np.array([[tT.cross_theil(tT.theilTree(bar[bar['year'] ==
+                                                    year], levels, groups),
+                                   firm)
+                    for year in bar.year.unique()]
+                   for firm in bar.firm.unique()])
--- treeTheil.py
+++ treeTheil.py
@@ -1,14 +1,13 @@
-from treelib import Tree
 from math import log
 from copy import deepcopy
-import pandas as pd
+from treelib import Tree
 
 ########################################################################
 # Functions and class for building a Theil tree
 ########################################################################
 
-# The idea here is that we often calculate Theil statistics from panel
-# data that has an implicit hierarchical structure. Thus for example
+# We often calculate Theil statistics from panel data that has an
+# implicit hierarchical structure. Thus for example
 #
 # Root, BranchA, LeafA, G1, G2
 # Root, BranchA, LeafB, G1, G2
@@ -16,7 +15,7 @@ import pandas as pd
 # Root, BranchB, LeafB, G1, G2
 # Root, BranchC, LeafA, G1, G2
 #
-# ...etc. We want a dat structure that's more like a tree:
+# ...etc. We want a data structure more like a tree:
 #
 # Root
 #  |-BranchA
@@ -28,19 +27,18 @@ import pandas as pd
 #  `-BranchC
 #    `-LeafA (G1, G2)
 #
-
 # ...etc. The Theil statistics can then be calculated recursively
 # across the nodes in this tree. The "Thile" class is the data
 # structure that is appended to the nodes.  It holds the group numbers
-# for that node, and can be queried for two attributes, its total size
-# and the entropy between the groups.
+# for that node and can be queried for its total size, its lowest
+# identifying unit, and the entropy between the groups.
 
 
 class Thile(object):
 
-    def __init__(self, groups, node_level):
+    def __init__(self, groups, lunit):
         self.groups = groups
-        self.node_level = node_level
+        self.lunit = lunit
 
     def total(self):
         total = sum(self.groups.values())
@@ -55,35 +53,40 @@ class Thile(object):
 
 
 def maybefloat(value):
-    ''' Tests whether a variable can be made into a float.  Returns a
-    float if it can, the original variable if it cannot. '''
+    '''Tests whether a variable can be made into a float.  Returns a
+    float if it can, the original variable if it cannot.
+
+    '''
     try:
         return float(value)
     except ValueError:
         return value
 
 
-def inc_dicts(fromDict, toDict):
-    ''' Adds values in a pair of dictionaries. '''
+def inc_dict(fromDict, toDict):
+    '''Adds values in a pair of dictionaries.'''
     for key in toDict.keys():
         toDict[key] += fromDict[key]
 
 
 def deepcopy_node_data(tree, fromNode, toNode):
-    ''' Copies data structure across nodes. Need to use deepcopy rather
+    '''Copies data structure across nodes. Need to use deepcopy rather
     than copy, because otherwise the pointer will be to the original
-    data. '''
+    data.
+
+    '''
     tree[toNode].data.groups = deepcopy(tree[fromNode].data.groups)
 
 
 def tree_structure(tree, dataframe, levels, datums):
-    ''' Single pass through the dataframe to create the tree structure.
+    '''Single pass through the dataframe to create the tree structure.
     Works down from the root, passing already-created nodes, and
     assigns the data to the leaf node.
 
-    Node IDs are created by concatenating values for the different levels,
-    starting from the root.  This avoids ambiguity when two nodes at the
-    same level might otherwise have the same name. '''
+    Node IDs concatenate values for the different levels (joined by
+    pipe characters), starting from the root.
+
+    '''
     for row in range(len(dataframe)):
         level_list = []
         for level in levels:
@@ -93,36 +96,37 @@ def tree_structure(tree, dataframe, levels, datums):
             leaf_data[datum] = maybefloat(dataframe[datum].iloc[row])
         for i in range(len(level_list)):
             node_id = '|'.join(level_list[:i+1])
-            node_level = level_list[i]
+            lunit = level_list[i]
             parent_id = '|'.join(level_list[:i])
             if tree.contains(node_id):
                 pass
             elif i == 0:
-                tree.create_node(node_id, node_id, data=Thile({},
-                                                              node_level))
+                tree.create_node(node_id, node_id, data=Thile({}, lunit))
             elif i == len(level_list)-1:
                 tree.create_node(node_id, node_id, parent=parent_id,
-                                 data=Thile(leaf_data, node_level))
+                                 data=Thile(leaf_data, lunit))
             else:
                 tree.create_node(node_id, node_id, parent=parent_id,
-                                 data=Thile({}, node_level))
+                                 data=Thile({}, lunit))
 
 
 def leaf_up_tree(tree):
-    ''' Single pass through leaves to build hierarchical sums of data up
-    through the tree. The leaf data dictionary is copied to the parent
-    node if the latter has no data, and added to the parent node's
-    data if it does. '''
+    '''Single pass through leaves to hierarchically sum data up the
+    tree. The child's data dictionary is deep-copied to the parent if
+    the latter has no data, and added to the parent node's data if it
+    does.
+
+    '''
     for path in tree.paths_to_leaves():
         leaf = path.pop()
         for parent in path:
             if tree[parent].data.groups == {}:
                 deepcopy_node_data(tree, leaf, parent)
             else:
-                inc_dicts(tree[leaf].data.groups, tree[parent].data.groups)
+                inc_dict(tree[leaf].data.groups, tree[parent].data.groups)
 
 
-def theil_tree(dataframe, levels, datums):
+def theilTree(dataframe, levels, datums):
     tree = Tree()
     tree_structure(tree, dataframe, levels, datums)
     leaf_up_tree(tree)
@@ -130,45 +134,41 @@ def theil_tree(dataframe, levels, datums):
 
 
 ########################################################################
-# Functions for working with Theil statistics
+# Functions for working with Theil trees
 ########################################################################
 
 
-# We often need an individual node's contribution to the Theil
-# statistic, which is defined on the higher level; we tend to call
-# this the "Theil statistic component."  It's the node's entropy
-# deviation, weighted by its size.  To calculate it, we need the total
-# and entropy properties of the node, plus those properties of its
-# parent.
+def node_weight(tree, node):
+    '''Node's weight as a share of its parent'''
+    return tree[node].data.total() / tree.parent(node).data.total()
+
+
+def node_entropy(tree, node):
+    '''Node's entropy deviation from its parent'''
+    return (0 if tree.parent(node).data.entropy() == 0 else
+            (tree.parent(node).data.entropy() - tree[node].data.entropy()) /
+            tree.parent(node).data.entropy())
+
+
 def theil_cmp(tree, node):
-    '''Individual node's contribution to the Theil statistic one level
-    higher. Calculated as the node's entropy deviation from its parent,
-    weighted by its size as a share of the parent.'''
-    weight = tree[node].data.total() / tree.parent(node).data.total()
-    ent_dev = (0 if tree.parent(node).data.entropy() == 0 else
-               (tree.parent(node).data.entropy() -
-                tree[node].data.entropy()) /
-               tree.parent(node).data.entropy())
-    theil_cmp = weight * ent_dev
-    return theil_cmp
+    '''Node's contribution to its parent's Theil statistic. Calculated as
+    the node's entropy deviation from its parent, weighted by its size
+    as a share of the parent.
+
+    '''
+    return node_weight(tree, node) * node_entropy(tree, node)
 
 
-# Now is where it gets good.  Because we have the data in a tree
-# structure, we can recur through levels of the tree.  This theil()
-# function does that.  Note that it returns zero on leaf nodes.  This
-# is as it should be.  A unit with a single sub-unit cannot be
-# segregated.  Letting it just return zeros on leaf nodes spares us a
-# ton of conditional branching to test for leaves without screwing up
-# the results of the calculation.
-
-# Notice that, while the theil_cmp() function leverages the parent()
-# attribute of the tree structure, the theil() function leverages the
-# child() attribute.  This is where putting this information in a tree
-# structure starts to pay off.  The relationship between different
-# levels is explicitly accounted for by the data structure.  We don't
-# need endless indices, bysorts, egens and the rest.
+# Because the data are in a tree structure, one can recur through
+# levels of the tree.  Note that this recursion returns zero on leaf
+# nodes.  This is as it should be.  A unit with a single sub-unit
+# cannot be segregated.  Letting the recursion terminate with the
+# additive identity spares a lot of conditional branching to test
+# whether nodes are leaves.
 def theil(tree, unit, levels):
-    ''' Size-weighted sum of entropy deviations of a unit's sub-units. '''
+    '''Size-weighted sum of entropy deviations of a parent's children.
+
+    '''
     the_theil = 0
     if levels == 0:
         for subunit in tree.children(unit):
@@ -180,3 +180,32 @@ def theil(tree, unit, levels):
             the_theil += sub_weight * theil(tree, subunit.identifier,
                                             levels - 1)
     return the_theil
+
+
+def btw_theil(tree, unit):
+    '''Between-child component of a parent's Theil statistic.'''
+    return theil(tree, unit, 0)
+
+
+# This version doesn't allow recursive Theils on the within
+# component. Come back to that.
+def win_theil(tree, sub_unit):
+    '''Within-child component of a parent's Theil statistic.  Called on a
+    specific child.
+
+    '''
+    return btw_theil(tree, sub_unit) * node_weight(tree, sub_unit)
+
+
+# This assumes that the lunit in question is on level 2. Come back to
+# that.
+def cross_theil(tree, lunit):
+    cross_theil = 0
+    for node in tree.all_nodes():
+        if tree[node.identifier].data.lunit == lunit:
+            cross_theil += (theil_cmp(tree, node.identifier) *
+                            node_weight(tree,
+                                        tree.parent(node.identifier).identifier))
+        else:
+            pass
+    return cross_theil
